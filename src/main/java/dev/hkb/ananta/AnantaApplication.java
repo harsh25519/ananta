5081029b130e938e18b414af59909bcdb024d8ca
HarshBhardwaj <harshbdj25@gmail.com>
2026-02-23 21:51:41 +0530

    Initial Spring Boot Setup

---
+++ src/main/java/dev/hkb/ananta/AnantaApplication.java
@@ -0,0 +1,13 @@
+package dev.hkb.ananta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnantaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hkb/ananta/AnantaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hkb.ananta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
