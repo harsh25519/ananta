20b1d345c8aaeb85ccbe25d654e4ccbde50e19d1
HarshBhardwaj <harshbdj25@gmail.com>
2026-05-28 18:25:53 +0530

    Updated CORS with some more local ports

--- src/main/java/dev/hkb/ananta/security/config/SecurityConfig.java
+++ src/main/java/dev/hkb/ananta/security/config/SecurityConfig.java
@@ -141,7 +141,10 @@ public class SecurityConfig{
 
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "http://localhost:3000"
+                "http://localhost:3000",
+                "http://127.0.0.1:3000",
+                "http://localhost:5500",
+                "http://127.0.0.1:5500"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
